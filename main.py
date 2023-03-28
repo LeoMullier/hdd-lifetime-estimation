040ba78fdb35f41d87aa2346c71dcd0c2fdff57a
Nicolas Parmentier <p.nicolasparmentier@gmail.com>
2023-03-28 10:49:53 +0200

    [+] Add main.py

---
+++ main.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import os
+import multiprocessing as mp
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm import tqdm
+
+
+def merge_dictionary(dict1, dict2):
+    merged_dict = dict1.copy()
+    for key, value in dict2.items():
+        if key in merged_dict:
+            if type(merged_dict[key]) is list:
+                merged_dict[key].append(value)
+            else:
+                merged_dict[key] = [merged_dict[key], value]
+        else:
+            merged_dict[key] = value
+    return merged_dict
+
+
+def csv_to_dataframe(csv_name) -> pd.DataFrame:
+    try:
+        dataframe = pd.read_csv('data/' + csv_name)
+        return dataframe
+    except (Exception,):
+        print(f'Cannot read {csv_name}')
+        return pd.DataFrame()
+
+
+def parse_file(file_name, serial_numbers):
+    interesting_rows = {}
+    new_serial_numbers = []
+    dataframe_it = csv_to_dataframe(file_name)
+
+    failures_dataframe = dataframe_it[(dataframe_it["failure"] == 1)]
+    if not failures_dataframe.empty:
+        for index, row in failures_dataframe.iterrows():
+            new_serial_number = dataframe_it.iloc[index]['serial_number']
+            if new_serial_number not in serial_numbers:
+                serial_numbers.append(new_serial_number)
+                new_serial_numbers.append(new_serial_number)
+
+    for serial_number in serial_numbers:
+        interesting_row = dataframe_it.loc[dataframe_it['serial_number'] == serial_number]
+        interesting_rows[serial_number] = interesting_row
+    return (interesting_rows, new_serial_numbers)
+
+
+def main():
+    # Variables
+    disk_data = pd.DataFrame()
+    file_list = os.listdir('data')
+    serial_numbers = []
+    history_length = 60
+
+    for idx, file in enumerate((sorted(file_list, reverse=True))):
+        print(f'Computing {file}')
+        if os.path.exists(f'results/{file[:10]}'):
+            print("Already done")
+            continue
+
+        dataframe = csv_to_dataframe(file)
+        failures_dataframe = dataframe[(dataframe["failure"] == 1)]
+
+        if not failures_dataframe.empty:
+            for index, row in failures_dataframe.iterrows():
+                serial_numbers.append(dataframe.iloc[index]['serial_number'])
+
+            if idx + history_length >= len(file_list):
+                print("Cannot continue, too few files to continue")
+                break
+
+            print(f'Serial Numbers with failures in {file} : {serial_numbers}')
+            print(f'Parsing history from {file[:10]} to {sorted(file_list, reverse=True)[idx+history_length][:10]}')
+            rows = {}
+
+            with ProcessPoolExecutor(max_workers=mp.cpu_count()) as executor:
+                futures = [executor.submit(parse_file, file_name, serial_numbers) for file_name in
+                           sorted(file_list, reverse=True)[idx:idx+60]]
+                for future in tqdm(as_completed(futures), total=len(futures)):
+                    try:
+                        data = future.result()
+                        if data is not None:
+                            rows = merge_dictionary(rows, data[0])
+                            serial_numbers
+                    except Exception as exc:
+                        print(f'Parsing generated an exception: {exc}')
+
+            for serial_number in serial_numbers:
+                print(f'Creating csv file for {serial_number}...')
+                disk_data = pd.concat(rows[serial_number], ignore_index=True)
+                disk_data['date'] = pd.to_datetime(disk_data['date'])
+                disk_data = disk_data.sort_values(by='date', ascending=False)
+                os.makedirs(f'results/{file[:10]}', exist_ok=True)
+                disk_data.to_csv(f'results/{file[:10]}/{serial_number}.csv', sep=",")
+                disk_data = pd.DataFrame()
+            print()
+
+
+if __name__ == '__main__':
+    main()
